ffaf63d7e46702e8172972a5cec67daecac0e642
erthalion <9erthalion6@gmail.com>
2014-11-01 12:58:29 +0700

    Split rundmenu & reindex dmenu

--- bash/reindex_dmenu.sh
+++ bash/reindex_dmenu.sh
@@ -1,13 +1,10 @@
 #!/bin/sh 
 
-PROGSFILE="/home/erthalion/.proglist"
+PROGSFILE="/home/erthalion/.proglist" 
 
 proglist() { 
         paths=$(echo "$@" | sed 'y/:/ /') 
         ls -lL $paths 2>/dev/null | awk '$1 ~ /^[^d].*x/ && NF > 2 { print $NF }' | sort | uniq
 } 
 
-if [ ! -f ~/.proglist ]; then
-    proglist $PATH >$PROGSFILE
-fi
-sh -c  "$(eval dmenu -b <$PROGSFILE)"
+proglist $PATH >$PROGSFILE 
