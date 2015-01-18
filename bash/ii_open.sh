9fb04895c8aac1fc85c2a2033e1c76e59bbde27a
erthalion <9erthalion6@gmail.com>
2015-01-18 22:21:50 +0700

    Set window name

--- bash/ii_open.sh
+++ bash/ii_open.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+tmux rename-window $1
 tmux splitw -v -p 30 'vim'
 multitail -cS ii ~/irc/irc.freenode.net/#$1/out
--- bash/ii_open_gitter.sh
+++ bash/ii_open_gitter.sh
@@ -2,5 +2,6 @@
 
 # multitail 6.2.1 with unicode use flag required for unicode support
 
+tmux rename-window $1
 tmux splitw -v -p 30 'vim'
 multitail -cS ii ~/irc/localhost/#$1/out
